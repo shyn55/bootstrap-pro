45a79983db89a98452d973b3bb12359a06990975
shayn.mir <shayan.mir1380@gmail.com>
2026-01-30 22:52:05 +0330

    first

---
+++ js codes/app.js
@@ -0,0 +1,6 @@
+const arrow=document.querySelector(".arrow")
+const shoeslist =document.querySelector(".shoes-list")
+
+shoeslist.addEventListener("click",()=>{
+    arrow.classList.toggle("arrowUp")
+})
